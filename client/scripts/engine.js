bd441bad3401d391b79dab4bca97aed77f4add59
3phase <pbozhinov@yahoo.com>
2016-03-14 22:36:45 +0200

    Cards separated according announce #7

--- client/scripts/engine.js
+++ client/scripts/engine.js
@@ -2,52 +2,59 @@ $(document).ready(function() {
 	"use strict";
 
 	var announce = null;
+	var player_turn = 0;
 
-	var colors = ["clubs", "diamonds", "hearts", "spades", "suit", "trump"];
+	var game_container = $(".game-container");
+	var colors = ["clubs", "diamonds", "hearts", "spades", "suit", "trumps"];
+	var players = ["south", "east", "north", "west"];
 
-	var cards_trump = {
-		"J": 20,
-		"9": 14,
-		"A": 11,
-		"10": 10,
-		"K": 4,
-		"Q": 3,
-		"8": 0,
-		"7": 0
-	};
+	var cards_trumps = ["J", "9", "A", "10", "K", "Q", "8", "7"];
 
-	var cards_suit = {
-		"A": 11,
-		"10": 10,
-		"K": 4,
-		"Q": 3,
-		"J": 2,
-		"9": 0,
-		"8": 0,
-		"7": 0	
-	};
+	var cards_suit = ["A", "10", "K", "Q", "J", "9", "8", "7"];
 
 	function process_announce(color) {
 		var color_idx = $.inArray(color, colors);
 		var buttons = $(".announce-color")
-		for (var i = 0; i < color_idx; i++) {
+		for (var i = 0; i <= color_idx; i++) {
 			var button_name = "." + colors[i];
 			$(button_name).prop("disabled", true);
 		}
-		announce = colors[color_idx];
+		if(color != "pass") {
+			announce = colors[color_idx];
+		}
 
 	}
 
-	var game_container = $(".game-container");
+	function distribute_cards() {
+		game_container.html("");
+		game_container.html("<strong>Announce: </strong>" + announce);
+		var real_colors = colors.slice(0,4);
+		var cards = {};
+		$.each(real_colors, function(key, value) {
+			if (announce == "trumps" || announce == value)
+				cards[value] = cards_trumps;
+			else
+				cards[value] = cards_suit;
+		});
+		$.each(cards, function(key, value) {
+			console.log(key + "=>" + value);
+		}); 
+	}
+
 	game_container.html("<strong>Announce:</strong><br>");
 	$.each(colors, function(key, value) {
 		game_container.append("<button class='announce-color " + value + "'>" + value + "</button>");
 	});
+	game_container.append("<button class='announce-color pass'>Pass</button>")
 
 	$(".announce-color").on("click", function() {
 		var attr = $(this).attr("class");
 		var color = attr.split(" ")[1];
 		process_announce(color);
+		player_turn += 1;
+		if (player_turn > 3) {
+			distribute_cards();
+		}
 	});
 
 });
