975c331996c8c90a22c7068d44fb002a687ad4d9
3phase <pbozhinov@yahoo.com>
2016-03-15 00:24:46 +0200

    Distribution is completed #7

--- client/scripts/engine.js
+++ client/scripts/engine.js
@@ -4,15 +4,20 @@ $(document).ready(function() {
 	var announce = null;
 	var player_turn = 0;
 
-	var game_container = $(".game-container");
 	var colors = ["clubs", "diamonds", "hearts", "spades", "suit", "trumps"];
-	var players = ["south", "east", "north", "west"];
+	var players = {
+		"south": [], 
+		"east": [], 
+		"north": [], 
+		"west": []
+	};
 
 	var cards_trumps = ["J", "9", "A", "10", "K", "Q", "8", "7"];
-
 	var cards_suit = ["A", "10", "K", "Q", "J", "9", "8", "7"];
 
-	function process_announce(color) {
+	var cards = {};
+
+	function processAnnounce(color) {
 		var color_idx = $.inArray(color, colors);
 		var buttons = $(".announce-color")
 		for (var i = 0; i <= color_idx; i++) {
@@ -25,15 +30,9 @@ $(document).ready(function() {
 
 	}
 
-	function distribute_cards() {
-		game_container.html("");
-		game_container.html("<strong>Announce: </strong>" + announce);
+	function distinguishCards() {
 		var real_colors = colors.slice(0,4);
-		var cards = {};
 		$.each(real_colors, function(key, value) {
-			if (announce == "trumps" || announce == value)
-				cards[value] = cards_trumps;
-			else
 				cards[value] = cards_suit;
 		});
 		$.each(cards, function(key, value) {
@@ -41,20 +40,51 @@ $(document).ready(function() {
 		}); 
 	}
 
+	function remove_card_from_common_deck(color, val) {
+		cards[color] = jQuery.grep(cards[color], function(value) {
+		  return value != val;
+		});
+	}
+
+	function getRandomCard() {
+		var real_colors = colors.slice(0,4);
+		var rand_color = real_colors[Math.floor(Math.random() * real_colors.length)];
+		var rand_color_cards = cards[rand_color];
+		var rand_card = rand_color_cards[Math.floor(Math.random() * rand_color_cards.length)];
+		remove_card_from_common_deck(rand_color, rand_card);
+		return(rand_color + ":" + rand_card);
+	}
+
+	function distributeCards(count) {
+		$.each(players, function(key, value) {
+			for (var i=0; i < count; i++) {
+				var card = getRandomCard();
+				players[key].push(card);
+			}
+		});
+		console.log(players);
+	}
+
+	var game_container = $(".game-container");
 	game_container.html("<strong>Announce:</strong><br>");
 	$.each(colors, function(key, value) {
 		game_container.append("<button class='announce-color " + value + "'>" + value + "</button>");
 	});
 	game_container.append("<button class='announce-color pass'>Pass</button>")
+	distinguishCards();
+	distributeCards(5);
 
 	$(".announce-color").on("click", function() {
 		var attr = $(this).attr("class");
 		var color = attr.split(" ")[1];
-		process_announce(color);
+		processAnnounce(color);
 		player_turn += 1;
 		if (player_turn > 3) {
-			distribute_cards();
+			game_container.html("");
+			game_container.html("<strong>Announce: </strong>" + announce);
+			distributeCards(3);
 		}
 	});
+
 
 });
