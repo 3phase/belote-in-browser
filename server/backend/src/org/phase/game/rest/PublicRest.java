0ace6c2e2de25d492927e7d93d381b9b2d4a740b
3phase <pbozhinov@yahoo.com>
2016-03-25 00:47:26 +0200

    #6 Working REST, hurray ! Tested with http://localhost:port/project/rest/task/helloWorld

--- server/backend/src/org/phase/game/rest/PublicRest.java
+++ server/backend/src/org/phase/game/rest/PublicRest.java
@@ -2,40 +2,23 @@ package org.phase.game.rest;
 
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 
-import org.phase.game.entities.Player;
-import org.phase.game.entities.Room;
-import org.phase.game.entities.Team;
 import org.phase.game.gamecontext.GameServices;
+
+import com.google.common.net.MediaType;
 
 @Path("/task")
 public class PublicRest {
 	
-	private final GameServices gameServices;
-	
-	@Inject
-	public PublicRest(GameServices gameSrv) {
-		this.gameServices = gameSrv;
-		System.out.println("Jajajaj");
+	@GET
+	@Path("/helloWorld")
+	@Produces("text/plain")
+	public String test() {
+		return("Hello world");
 	}
-	
-	@POST
-	@Produces({MediaType.APPLICATION_JSON})
-	@Consumes({MediaType.APPLICATION_JSON})
-	public Room createNewRoom(final MyJaxBean object) throws Exception {
-		System.out.println("HAHAHAHAHA");
-		Room room = new Room();
-		Player player = object.param1;
-		Team team = new Team();
-		team.new_player(player);
-		room.add_team(team);
-		return room;
-	}
-	
 	
 	// TODO: addPlayerToTeam
 	// TODO: createNewRoom	
