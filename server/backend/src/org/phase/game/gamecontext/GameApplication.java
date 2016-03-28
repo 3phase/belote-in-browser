be7ae49ea8b098e323065f3d920b2aca3a49b1ab
3phase <pbozhinov@yahoo.com>
2016-03-28 20:08:12 +0300

    #6 Fixed @Inject problem, hurray

--- server/backend/src/org/phase/game/gamecontext/GameApplication.java
+++ server/backend/src/org/phase/game/gamecontext/GameApplication.java
@@ -6,6 +6,7 @@ import org.glassfish.hk2.api.ServiceLocator;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.jvnet.hk2.guice.bridge.api.GuiceBridge;
 import org.jvnet.hk2.guice.bridge.api.GuiceIntoHK2Bridge;
+import org.phase.game.GameListServletContextListener;
 
 class GameApplication extends ResourceConfig {
 	
