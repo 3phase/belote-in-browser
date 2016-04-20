c268faa46524c7bb035ffce59d9e721a05582230
3phase <pbozhinov@yahoo.com>
2016-04-21 00:20:50 +0300

    #27 Waiting for the exact amount of players to join in order to continue

--- server/backend/src/main/webapp/scripts/engine-new.js
+++ server/backend/src/main/webapp/scripts/engine-new.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
+	
 	"use strict";
 	
 	var roomStatus = false;
 	var roomId = -1;
 	var userId = -1;
+	
 	
 	function checkIfRoomDone() {
 		$.ajax({
@@ -13,8 +15,10 @@ $(document).ready(function() {
 				xhr.setRequestHeader("accept", "text/plain");
 			},
 			success: function(result) {
-				if (result == 1) {
+				if (result == 1) { 
 					roomStatus = true;
+					clearInterval(interval);
+					$(".game-container").html("Dobar den Evropa");
 				}
 			},
 			error: function(xhr, status, error) {
@@ -24,20 +28,24 @@ $(document).ready(function() {
 	}
 	
 	
-	if (Cookies.get("user-token") == undefined || Cookies.get("room-token") == undefined) {
+	if (Cookies.get("user-token") === undefined || Cookies.get("room-token") === undefined) {
 		$(".game-container").html("");
 		$(".game-container").html("Fill your <a href=\"initial_room.html\">data</a> first.")
 		return false;
 	}
+	
 	roomId = Cookies.get("room-token");
 	userId = Cookies.get("user-token");
 	
+	var counter = 0;
+//	while (roomStatus == false) {
+	// Waiting for fourth person to join
 	
-	while (roomStatus == false) {
-		// Waiting for fourth person to join
+	if (roomStatus == false) {
 		$(".game-container").html("");
 		$(".game-container").html("Waiting for the required amount of players to gather...");
-		setInterval(checkIfRoomDone, 1000);
+		var interval = setInterval(checkIfRoomDone, 5000);
 	}
+//	}
 	
 });
