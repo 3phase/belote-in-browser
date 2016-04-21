936404b59c7e25b074869e9173a3ee55d9094e46
3phase <pbozhinov@yahoo.com>
2016-04-21 16:21:05 +0300

    #19 Setting cookie when creating a room, which would be later read by engine.js

--- server/backend/src/main/webapp/scripts/init_room.js
+++ server/backend/src/main/webapp/scripts/init_room.js
@@ -3,10 +3,27 @@ $(document).ready(function() {
 
 	function createRoom() {
 		var actionField = $(".play-options");
-		
+		var roomId = -1;
 		
 		actionField.html("");
-		actionField.append("Room number X was created. You can now <a href=\"game.html\">start playing</a>...");
+		
+		$.ajax({
+			type: "GET",
+			url: "http://127.0.0.1:8080/05_SampleBackend/rest/play/room/create",
+			beforeSend: function(xhr) {
+				xhr.setRequestHeader("accept", "text/plain");
+			},
+			success: function(result) {
+				roomId = result;
+				actionField.append("Room with ID <strong>" + roomId + "</strong> was created. You can now <a href=\"game.html\">start playing</a>...");
+				Cookies.set("room-token", roomId, {path: "game.html"});
+				console.log(Cookies.get("room-token"));
+			},
+			error: function(xhr, status, error) {
+				actionField.append("Problem while creating room. " + JSON.stringify(xhr) + "; " + status + "; " + error);
+			}
+		});
+		
 	}
 	
 	function joinRoom() {
